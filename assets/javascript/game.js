58fff1fe541af6c530bd36f310ee52145ba7c208
Andre <amcaron1@msn.com>
2018-09-24 20:49:03 -0500

    Final copy

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,6 @@
-var crystals = [
+$(document).ready(function() {
+
+  var crystals = [
     {name: "diamond", picture: "assets/images/diamond1.png"},
     {name: "emerald", picture: "assets/images/emerald1.png"},
     {name: "garnet", picture: "assets/images/garnet1.png"},
@@ -11,51 +13,40 @@ var crystals = [
   var gameOver = false;
   var targetNumber = Math.floor(Math.random() * 101) + 20;
 
+  // Creates target number.
   $("#targetNumberH").text(targetNumber);
  
-
-
-  // Now for the hard part. Creating multiple crystals each with their own unique number value.
-
-  // Next we create a for loop to create each crystal
+  // Creates each crystal
   for (var i = 0; i < crystals.length; i++) {
 
-    // For each iteration, we will create an imageCrystal
+    // For each iteration, an imageCrystal is created.
     var imageCrystal = $("<img>");
 
-    // First each crystal will be given the class ".crystal-image".
+    // First each crystal is given the class ".crystal-image".
     // This will allow the CSS to take effect.
     imageCrystal.addClass("crystal-image");
 
-    // Each imageCrystal will be given a src link to the crystal image
+    // Each imageCrystal is given a src link to the crystal image
     imageCrystal.attr("src", crystals[i].picture);
 
-    // Each imageCrystal will be given a data attribute called data-crystalValue.
+    // Each imageCrystal is given a data attribute called data-crystalValue.
     imageCrystal.attr("data-crystalvalue", Math.floor(Math.random() * 11) + 1);
 
-
+    // Each imageCrystal is given an id in order to reset the data-crystalvalue.
     imageCrystal.attr("id", i);
 
-  
-    // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
+    // Lastly, each crystal image (with all it classes and attributes) is added to the page.
     $("#crystals").append(imageCrystal);
 
   }
     
-
-
-
-  // This time, our click event applies to every single crystal on the page. Not just one.
-  // $(".crystal-image").on("click", function() {
-
-  // for handling elements that are created after the jQuery
+  // The click event applies to every single crystal on the page. Not just one.
   $(document).on("click", ".crystal-image", function() {
     
     // Determining the crystal's value requires us to extract the value from the data attribute.
     // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
     // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
     // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
-
     if (gameOver == false) {
       var crystalValue = ($(this).attr("data-crystalvalue"));
       crystalValue = parseInt(crystalValue);
@@ -63,26 +54,28 @@ var crystals = [
       // Every click, from every crystal adds to the global counter.
       crystalCounter += crystalValue;
 
-      // All of the same game win-lose logic applies. So the rest remains unchanged.
+      // The counter is updated on the screen.
       $("#crystalCounterH").text(crystalCounter);
-      // alert("New score: " + crystalCounter);
 
+      // If the player matches the target number, then the win counter is incremented and a win message is displayed
       if (crystalCounter === targetNumber) {
         winsCounter += 1
         $("#winsCounterH").text(winsCounter);
-        $("#messageH").text("You win!  Click Reset to start a new game.");
+        $("#messageH").html("You win! &nbsp; &nbsp; Click Reset to start a new game.");
         gameOver = true;
       }
 
-    if (crystalCounter > targetNumber) {
+      // If the player exceeds the target number, then the loss counter is incremented and a loss message is displayed
+      if (crystalCounter > targetNumber) {
         lossesCounter += 1
         $("#lossesCounterH").text(lossesCounter);
-        $("#messageH").text("Sorry, you lost!  Click Reset to start a new game.");
+        $("#messageH").html("Sorry, you lost! &nbsp; &nbsp; Click Reset to start a new game.");
         gameOver = true;
       }
     }
   });
 
+  // This code resets the game when the reset button is pressed after a win or loss
   $(document).on("click", "#resetButtonH", function() {
     console.log("gameOver = " + gameOver);
     if (gameOver) {
@@ -99,3 +92,5 @@ var crystals = [
       }
     }
   })
+
+})
